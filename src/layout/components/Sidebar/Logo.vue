8092b813875ef951d4d40a4833c56bd110c122a8
L-xiaoli <625529828@qq.com>
2021-03-19 11:01:54 +0800

    完成主页的左侧导航样式

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,13 +1,8 @@
 <template>
-  <div class="sidebar-logo-container" :class="{'collapse':collapse}">
+  <div class="sidebar-logo-container" :class="{ collapse: collapse }">
     <transition name="sidebarLogoFade">
-      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
-        <h1 v-else class="sidebar-title">{{ title }} </h1>
-      </router-link>
-      <router-link v-else key="expand" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
-        <h1 class="sidebar-title">{{ title }} </h1>
+      <router-link key="collapse" class="sidebar-logo-link" to="/">
+        <img src="@/assets/common/logo.png" class="sidebar-logo" />
       </router-link>
     </transition>
   </div>
@@ -22,7 +17,7 @@ export default {
       required: true
     }
   },
-  data() {
+  data () {
     return {
       title: 'Vue Admin Template',
       logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
@@ -46,7 +41,7 @@ export default {
   width: 100%;
   height: 50px;
   line-height: 50px;
-  background: #2b2f3a;
+  // background: #2b2f3a;
   text-align: center;
   overflow: hidden;
 
@@ -54,9 +49,9 @@ export default {
     height: 100%;
     width: 100%;
 
-    & .sidebar-logo {
-      width: 32px;
-      height: 32px;
+    // 大图样式
+    .sidebar-logo {
+      width: 140px;
       vertical-align: middle;
       margin-right: 12px;
     }
@@ -72,10 +67,12 @@ export default {
       vertical-align: middle;
     }
   }
-
+  // 小图样式
   &.collapse {
     .sidebar-logo {
       margin-right: 0px;
+      width: 32px;
+      height: 32px;
     }
   }
 }
--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-17 13:50:19
+ * @LastEditTime: 2021-03-19 10:49:37
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \01_vue-admin-template\src\settings.js
+ */
 module.exports = {
-  title: "人力资源管理系统",
+  title: '人力资源管理系统',
 
   /**
    * @type {boolean} true | false
@@ -11,5 +19,5 @@ module.exports = {
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
-  sidebarLogo: false
-};
+  sidebarLogo: true // 显示logo
+}
