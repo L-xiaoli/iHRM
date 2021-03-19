a8c96909bd979ea64a80ef17b232233bbd6dcfba
L-xiaoli <625529828@qq.com>
2021-03-19 10:44:58 +0800

    完成主页中根据有无token，进行页面访问的处理

---
+++ src/permission.js
@@ -0,0 +1,33 @@
+import router from '@/router' // 引入路由实例
+import store from '@/store' // 引入vuex store实例
+import NProgress from 'nprogress' // 引入一份进度条插件
+import 'nprogress/nprogress.css' // 引入进度条样式
+
+const whiteList = ['/login', '/404'] // 定义白名单  所有不受权限控制的页面
+// 路由的前置守卫
+router.beforeEach(function(to, from, next) {
+  NProgress.start() // 开启进度条
+  //  首先判断有无token
+  if (store.getters.token) {
+    //? 1.有token 继续判断是不是去登录页
+    if (to.path === '/login') {
+      //  表示去的是登录页
+      next('/') // 跳到主页
+    } else {
+      next() // 直接放行
+    }
+  } else {
+    // ? 2.没有token
+    if (whiteList.indexOf(to.path) > -1) {
+      // 如果找到了 表示在在名单里面
+      next() // 放行
+    } else {
+      next('/login') // 重新到登录页
+    }
+  }
+  NProgress.done() // 手动强制关闭一次  为了解决 手动切换地址时  进度条的不关闭的问题
+})
+// 后置守卫
+router.afterEach(function() {
+  NProgress.done() // 关闭进度条
+})
