c9b4bd9a9992d4bc599b35b4c3bbd3b16d1a4b9c
L-xiaoli <625529828@qq.com>
2021-03-20 21:49:31 +0800

    封装组织架构中应用操作栏组件

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-row
+    type="flex"
+    justify="space-between"
+    align="middle"
+    style="height: 40px; width: 100%"
+  >
+    <el-col>
+      <!-- 名称应该变成 对应的节点中的name -->
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <!-- 两个内容 -->
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <!-- 下拉菜单 element -->
+          <el-dropdown>
+            <span> 操作<i class="el-icon-arrow-down" /> </span>
+            <!-- 下拉菜单 -->
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <span v-if="isRoot">
+                <el-dropdown-item>编辑部门</el-dropdown-item>
+                <el-dropdown-item>删除部门</el-dropdown-item>
+              </span>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    treeNode: {
+      type: Object, // 对象类型
+      required: true  //必传
+    },
+    isRoot: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,98 +1,36 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <!-- 组织架构内容--头部 -->
+      <!-- 实现页面的基本布局 -->
       <el-card class="tree-card">
-        <el-row
-          type="flex"
-          justify="space-between"
-          align="middle"
-          style="height: 40px"
-        >
-          <!-- 左侧内容 -->
-          <el-col><span>江苏传智播客教育科技股份有限公司</span></el-col>
-          <!-- / 左侧内容 -->
-          <!-- 右侧内容 -->
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <el-col>负责人</el-col>
-              <el-col>
-                <!-- 下拉菜单 -->
-                <el-dropdown>
-                  <span> 操作<i class="el-icon-arrow-down" /> </span>
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-                <!-- / 下拉菜单 -->
-              </el-col>
-            </el-row>
-          </el-col>
-          <!-- / 右侧内容 -->
-        </el-row>
-        <!-- 树形组件 -->
-        <el-tree
-          :data="departs"
-          :props="defaultProps"
-          @node-click="handleNodeClick"
-          :default-expand-all="true"
-        >
-          <!-- 自定义插槽 -->
-          <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
-          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
-          <el-row
-            type="flex"
-            justify="space-between"
-            align="middle"
-            style="width: 100%"
-            slot-scope="{ data }"
-          >
-            <!-- 左侧内容 -->
-            <el-col
-              ><span>{{ data.name }}</span></el-col
-            >
-            <!-- / 左侧内容 -->
-            <!-- 右侧内容 -->
-            <el-col :span="4">
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <el-col>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown>
-                    <span> 操作<i class="el-icon-arrow-down" /> </span>
-                    <!-- 具名插槽 -->
-                    <el-dropdown-menu slot="dropdown">
-                      <!-- 下拉选项 -->
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                  <!-- / 下拉菜单 -->
-                </el-col>
-              </el-row>
-            </el-col>
-            <!-- /右侧内容 -->
-          </el-row>
-
-          <!-- / 自定义插槽 -->
+        <tree-tools :tree-node="company" :is-root="isRoot" />
+        <el-tree :data="departs" :props="defaultProps" default-expand-all>
+          <!-- 说明el-tree里面的这个内容 就是插槽内容 => 填坑内容  => 有多少个节点循环多少次 -->
+          <!-- scope-scope 是 tree组件传给每个节点的插槽的内容的数据 -->
+          <tree-tools slot-scope="{ data }" :tree-node="data" />
         </el-tree>
-        <!-- / 树形组件 -->
       </el-card>
     </div>
   </div>
 </template>
 
+
 <script>
+import TreeTools from './components/tree-tools'
 export default {
+  components: {
+    TreeTools
+  },
   data() {
     return {
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
       departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
       { name: '行政部', manager: '刘备' },
       { name: '人事部', manager: '孙权' }],
       defaultProps: {
         label: 'name' // 表示 从这个属性显示内容
-      }
+      },
+      isRoot: false // 控制编辑和删除部门下拉菜单是否显示
     }
   },
   methods: {
