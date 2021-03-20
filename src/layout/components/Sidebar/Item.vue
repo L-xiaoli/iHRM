30ec8fbacd95054c0cc791b77ec269294ba37825
L-xiaoli <625529828@qq.com>
2021-03-20 16:15:05 +0800

    了解函数式组件

--- src/layout/components/Sidebar/Item.vue
+++ src/layout/components/Sidebar/Item.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
   name: 'MenuItem',
-  functional: true,
+  functional: true, // 表示该组件为一个函数式组件：没有data状态，没有响应式数据，只会接收props属性， 没有this， 他就是一个函数
   props: {
     icon: {
       type: String,
@@ -20,7 +20,7 @@ export default {
       if (icon.includes('el-icon')) {
         vnodes.push(<i class={[icon, 'sub-el-icon']} />)
       } else {
-        vnodes.push(<svg-icon icon-class={icon}/>)
+        vnodes.push(<svg-icon icon-class={icon} />)
       }
     }
 
