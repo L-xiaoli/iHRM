b3f2ad62f00b673b3c438fd0889daec22f8bd0ab
L-xiaoli <625529828@qq.com>
2021-03-28 11:14:50 +0800

    上传的进度条显示

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -16,6 +16,14 @@
     >
       <i class="el-icon-plus" />
     </el-upload>
+    <!-- 进度条 -->
+    <el-progress
+      v-if="showPercent"
+      style="width: 180px"
+      :percentage="percent"
+    />
+    <!-- 放置一个弹层 -->
+    <!-- sync修饰符自动将弹层关闭了 -->
     <el-dialog title="图片预览" :visible.sync="showDialog">
       <img :src="imgUrl" style="width:100%" alt="" />
     </el-dialog>
@@ -24,8 +32,8 @@
 <script>
 import COS from 'cos-js-sdk-v5' // 引入腾讯云的包
 const cos = new COS({
-  SecretId: 'AKIDDCWcfTZPWN1ScYhQTGmDWZO6W8TPgB9r',
-  SecretKey: 'b3KMFclfKnNltwnPvJ27xXr2nLQ1we5s'
+  SecretId: 'AKIDVkiPvG5IEvMWP2eP9zZyPhmcBSoteEVW',
+  SecretKey: 'yiBrITKfeNs5vaniNeeOr3d3FAoCixwk'
 })
 export default {
   data() {
@@ -33,7 +41,8 @@ export default {
       fileList: [], // 图片地址设置为数组
       showDialog: false, // 控制显示弹层
       imgUrl: '',
-      currentFileUid: null // 记录当前上传照片的uid
+      currentFileUid: null, // 记录当前上传照片的uid
+      showPercent: false // 默认不显示进度条
     }
   },
   computed: {
@@ -45,8 +54,8 @@ export default {
   methods: {
     // 点击预览事件
     preview(file) {
-      console.log(file.url)
       this.imgUrl = file.url
+      this.showDialog = true
       this.showDialog = true
     },
     handleRemove(file, fileList) {
@@ -69,30 +78,55 @@ export default {
         this.$message.error('图片大小最大不能超过5M')
         return false
       }
-      console.log(file)
       this.currentFileUid = file.uid
+      this.showPercent = true
       return true
     },
     // 进行上传操作
     upload(params) {
+      //   console.log(params.file)
       if (params.file) {
+        // 执行上传操作
         cos.putObject(
           {
-            Bucket: 'xiaoshayu63-1305398197', // 存储桶
-            Region: 'ap-beijing', // 地域
+            Bucket: 'lxl-1216-1305395351', // 存储桶
+            Region: 'ap-chengdu', // 地域
             Key: params.file.name, // 文件名
             Body: params.file, // 要上传的文件对象
-            StorageClass: 'STANDARD' // 上传的模式类型 直接默认 标准模式即可
+            StorageClass: 'STANDARD', // 上传的模式类型 直接默认 标准模式即可
+            // 上传到腾讯云 =》 哪个存储桶 哪个地域的存储桶 文件  格式  名称 回调
+            // 进度条
+            onProgress: params => {
+              this.percent = params.percent * 100
+            }
           },
-          function(err, data) {
+          (err, data) => {
+            // data返回数据之后 应该如何处理
             console.log(err || data)
-            if (!err && data.statusCode === 2000) {
-              this.fileList.map(item => {
+            // data中有一个statusCode === 200 的时候说明上传成功
+            if (!err && data.statusCode === 200) {
+              //   此时说明文件上传成功  要获取成功的返回地址
+              // fileList才能显示到上传组件上 此时我们要将fileList中的数据的url地址变成 现在上传成功的地址
+              // 目前虽然是一张图片 但是请注意 我们的fileList是一个数组
+              // 需要知道当前上传成功的是哪一张图片
+              this.fileList = this.fileList.map(item => {
+                // 去找谁的uid等于刚刚记录下来的id
                 if (item.uid === this.currentFileUid) {
-                  return { url: 'http://' + data.Location }
+                  // 将成功的地址赋值给原来的url属性
+                  return { url: 'http://' + data.Location, upload: true }
+                  // upload 为true 表示这张图片已经上传完毕 这个属性要为我们后期应用的时候做标记
+                  // 保存  => 图片有大有小 => 上传速度有快又慢 =>要根据有没有upload这个标记来决定是否去保存
                 }
                 return item
               })
+              setTimeout(() => {
+                this.showPercent = false // 隐藏进度条
+                this.percent = 0 // 进度归0
+              }, 2000)
+
+              // 将腾讯云地址写入到fileList上 ，保存的时候 就可以从fileList中直接获取图片地址
+
+              // 此时注意，我们应该记住 当前上传的是哪个图片  上传成功之后，将图片的地址赋值回去
             }
           }
         )
