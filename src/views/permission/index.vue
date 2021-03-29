71190c230004201f9ac4db560827d8e886749065
L-xiaoli <625529828@qq.com>
2021-03-29 16:38:12 +0800

    获取权限数据并转化树形

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,16 +1,74 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>权限管理</h2>
+      <!-- 靠右的按钮 -->
+      <page-tools>
+        <template v-slot:after>
+          <el-button type="primary" size="small">添加权限</el-button>
+        </template>
+      </page-tools>
+      <!-- 表格 -->
+      <el-table
+        border
+        :data="list"
+        default-expand-all
+        row-key="id"
+        :tree-props="{ children: 'children', hasChildren: 'pid!==0' }"
+      >
+        <el-table-column align="center" label="名称" prop="name" />
+        <el-table-column align="center" label="标识" prop="code" />
+        <el-table-column align="center" label="描述" prop="description" />
+        <el-table-column align="center" label="操作">
+          <template slot-scope="{ row }">
+            <el-button v-if="row.type === 1" type="text">添加</el-button>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permission'
+import { treeData } from '@/utils'
 export default {
-
+  data() {
+    return {
+      list: [],
+      formData: {
+        name: '', // 名称
+        code: '', // 标识
+        description: '', // 描述
+        type: '', // 类型 该类型 不需要显示 因为点击添加的时候已经知道类型了
+        pid: '', // 因为做的是树 需要知道添加到哪个节点下了
+        enVisible: '0' // 开启
+      },
+      rules: {
+        name: [
+          { required: true, message: '权限名称不能为空', trigger: 'blur' }
+        ],
+        code: [{ required: true, message: '权限标识不能为空', trigger: 'blur' }]
+      },
+      showDialog: false
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  computed: {
+    showText() {
+      return this.formData.id ? '编辑' : '新增'
+    }
+  },
+  methods: {
+    async getPermissionList() {
+      this.list = treeData(await getPermissionList(), '0')
+      console.log(this.list)
+    }
+  }
 }
 </script>
 
-<style>
-</style>
+<style></style>
