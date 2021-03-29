6bc8675fd794e8c9c8a226d5dd735ce17a853f00
L-xiaoli <625529828@qq.com>
2021-03-29 16:46:43 +0800

    新增编辑权限的弹层

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -11,7 +11,6 @@
       <el-table
         border
         :data="list"
-        default-expand-all
         row-key="id"
         :tree-props="{ children: 'children', hasChildren: 'pid!==0' }"
       >
@@ -21,12 +20,54 @@
         <el-table-column align="center" label="操作">
           <template slot-scope="{ row }">
             <el-button v-if="row.type === 1" type="text">添加</el-button>
-            <el-button type="text">编辑</el-button>
-            <el-button type="text">删除</el-button>
+            <el-button type="text" @click="editPermission(row.id)"
+              >编辑</el-button
+            >
+            <el-button type="text" @click="editPermission(row.id)"
+              >删除</el-button
+            >
           </template>
         </el-table-column>
       </el-table>
     </div>
+    <!-- 放置一个弹层 用来编辑新增节点 -->
+    <el-dialog
+      :title="`${showText}权限点`"
+      :visible="showDialog"
+      @close="btnCancel"
+    >
+      <!-- 表单 -->
+      <el-form
+        ref="perForm"
+        :model="formData"
+        :rules="rules"
+        label-width="120px"
+      >
+        <el-form-item label="权限名称" prop="name">
+          <el-input v-model="formData.name" style="width:90%" />
+        </el-form-item>
+        <el-form-item label="权限标识" prop="code">
+          <el-input v-model="formData.code" style="width:90%" />
+        </el-form-item>
+        <el-form-item label="权限描述">
+          <el-input v-model="formData.description" style="width:90%" />
+        </el-form-item>
+        <el-form-item label="开启">
+          <el-switch
+            v-model="formData.enVisible"
+            active-value="1"
+            inactive-value="0"
+          />
+          <!-- 1:开启  0：不开启 -->
+        </el-form-item>
+      </el-form>
+      <el-row slot="footer" type="flex" justify="center">
+        <el-col :span="6">
+          <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+          <el-button size="small" @click="btnCancel">取消</el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 
@@ -66,6 +107,18 @@ export default {
     async getPermissionList() {
       this.list = treeData(await getPermissionList(), '0')
       console.log(this.list)
+    },
+    // 编辑权限
+    editPermission(id) {
+      this.showDialog = true
+      console.log(id)
+    },
+    btnOK() {
+      this.showDialog = false
+    },
+    //  关闭弹层
+    btnCancel() {
+      this.showDialog = false
     }
   }
 }
